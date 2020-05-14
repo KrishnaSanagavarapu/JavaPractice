18b4e720077fba5adfbfcb05770814f2b734fd7c
krishna <krishna.javadev12@gmail.com>
2020-05-13 21:16:16 -0400

    first-commit

---
+++ JavaTpoint/src/com/practice/oops/Oops1.java
@@ -0,0 +1,74 @@
+package com.practice.oops;
+
+public class Oops1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+/**
+
+Java Notes
+
+Packages in Java
+
+- Package in java is a mechanism to encapsulate a group of classes, sub classes, sub packages and interfaces. 
+-A java package is a group of similar types of classes,interfaces and sub packages
+
+There are two types of packages
+1.User Defined Packages
+2.InBuilt Packages
+
+Examples of Built-in Packages
+
+some built in packages in java are lang,awt,javax,swing,net,io,util,sql,etc
+
+
+Advantages of Packages
+-Resusability
+-Better Organisation of Code
+-Name Conficts
+
+What is OOPS ? Define OOPS?
+OOPs - refers to a programming methodology based on objects instead of just functions and procedures.
+
+OOPs Concepts?
+
+Abstraction
+-Abstraction is the concept of hiding the internal details and describing things in simple terms.
+
+Encapsulation
+-Encapsulation is the technique used to implement abstraction in object-oriented programming. 
+-Encapsulation is used for access restriction to class members and methods
+-Access Modifiers - Privat,Public,Protected,Default
+
+Polymorphism
+-Polymorphism is the concept where an object behaves differently in different situations. 
+-There are two types of polymorphism – compile time polymorphism and runtime polymorphism.
+-Compile-time polymorphism is achieved by method overloading.
+-Runtime polymorphism is implemented when we have “IS-A” relationship between objects.
+
+Inheritance
+-Inheritance is the mechanism of code reuse. The object that is getting inherited is called superclass and the object that inherits the superclass is called subclass.
+-We use extends keyword in java to implement inheritance.
+
+Association
+-Association refers to the relationship between multiple objects. 
+-It refers to how objects are related to each other and how they are using each other's functionality. -Composition and aggregation are two types of association.
+
+Aggregation
+Aggregation is a weak association.
+Aggregation is also a “has-a” relationship. What distinguishes it from composition, that it doesn't involve owning. 
+
+ Composition
+The composition is the strong type of association
+Composition is a strong kind of “has-a” relationship because the containing object owns it.
+It means that if we destroy the owner object, its members also will be destroyed with it.
+
+
+
+**/
